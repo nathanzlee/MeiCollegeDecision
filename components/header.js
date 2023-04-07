981873c3d2f8f37d87ebc5ea62ce2ca6a14f43f9
Nathan Lee <worldnumber1pingpongplayer@gmail.com>
2023-04-07 00:22:43 -0700

    more tests again

--- components/header.js
+++ components/header.js
@@ -2,42 +2,42 @@ export default function Header() {
     return (
         <div className="w-full h-[80px] flex justify-evenly items-center">
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cal.png"
                 alt="Your Company"
             />
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cmu2.png"
                 alt="Your Company"
             />
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cal.png"
                 alt="Your Company"
             />
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cmu2.png"
                 alt="Your Company"
             />
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cal.png"
                 alt="Your Company"
             />
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cmu2.png"
                 alt="Your Company"
             />
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cal.png"
                 alt="Your Company"
             />
             <img
-                className="block h-[80px] w-auto"
+                className="block w-[12.5%] max-w-[80px] h-auto"
                 src="/cmu2.png"
                 alt="Your Company"
             />
